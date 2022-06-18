4ad61177b307548daf676c6a32644f2372bdc729
hanjun <654777646@qq.com>
2022-06-18 18:15:11 +0800

    add http request

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
-import qs from "qs";
 import { cleanObject, useDebounce } from "../../utils/index";
 import { useMount } from "../../utils/index";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
+import { useHttp } from "utils/http";
 
-const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
@@ -14,23 +13,30 @@ export const ProjectListScreen = () => {
   const debounceParam = useDebounce(param, 1000);
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
+    client("projects", { data: cleanObject(debounceParam) }).then((param) => {
+      console.log(param);
     });
+    // fetch(
+    //   `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
+    // ).then(async (response) => {
+    //   if (response.ok) {
+    //     setList(await response.json());
+    //   }
+    // });
   }, [debounceParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
+    client("users").then((param) => {
+      console.log(param);
     });
+    // fetch(`${apiUrl}/users`).then(async (response) => {
+    //   if (response.ok) {
+    //     setUsers(await response.json());
+    //   }
+    // });
   });
   return (
     <div>
